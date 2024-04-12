e759e457bf19fb8b354d81d79fdd6cbc6e635241
nokibh <nokibhowlader2003@gmail.com>
2024-04-12 11:44:09 +0600

    added login

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,29 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+      {
+        path: '/login',
+        element: <Login></Login>,
+      },
+      {
+        path: '/register',
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
